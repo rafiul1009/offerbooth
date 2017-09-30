58b94767329bcf5323553be8aea33a829b52e41e
Md. Rafiul Islam <rafiul1009@gmail.com>
2017-09-30 14:19:31 +0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,10 +7,10 @@
         // $(".image-list").masonry();
 
 
-        $(window).bind("scroll", function () {
+        /* $(window).bind("scroll", function () {
             $(window).height();
             $(window).scrollTop() > 100 && $(".header-area").addClass("fixed"), $(window).scrollTop() < 111 && $(".header-area").removeClass("fixed")
-        });
+        }); */
 
     });
 
